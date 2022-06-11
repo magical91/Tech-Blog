41345b4997faea9be31ac40a943cc86cc841fb5a
magicalbeauty91 <magicalbeauty91@gmail.com>
2022-06-11 15:52:19 -0500

    added User model

---
+++ models/User.js
@@ -0,0 +1,38 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class User extends Model { }
+// define table columns and configuration
+User.init(
+    {
+        // define an id column
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        // define a username column
+        username: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        // define a password column
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                len: [4]
+            }
+        }
+    },
+    {
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'user'
+    }
+);
+
+module.exports = User;
