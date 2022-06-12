bfb3224851ebe758ba822b63f9341a7ebd21085d
magicalbeauty91 <magicalbeauty91@gmail.com>
2022-06-12 09:47:24 -0500

    populated the template with sequelize data

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,7 +1,25 @@
 const router = require('express').Router();
+const sequelize = require('../config/connection');
+const { Post, User, Comment } = require('../models');
 
 router.get('/', (req, res) => {
-    res.render('homepage');
+    Post.findAll({
+        attributes: [
+            'id',
+            'title',
+            'created_at'
+        ],
+        order: [['created_at', 'DESC']]
+    })
+        .then(dbPostData => {
+            const posts = dbPostData.map(post => post.get({ plain: true }))
+            // pass a single post object into the homepage template
+            res.render('homepage', { posts })
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
 });
 
 module.exports = router;
